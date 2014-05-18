4a8c0d8dab1ae4433e426c36a90ecaff5fd78d3d
Mart van de Ven <m@type.hk>
2014-05-18 14:43:01 +0800

    Default global username

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name='OpenPlatform',
-    version='0.0.1',
+    version='0.0.2',
     author='Mart van de Ven',
     author_email='m@type.hk',
     packages=['openplatform'],
